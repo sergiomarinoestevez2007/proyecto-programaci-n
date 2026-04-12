14d04acf1d7bf174c481c067e78f06f34107585a
oscareduchav <oscareduchav@gmail.com>
2026-04-12 11:45:23 +0200

    commit de Juego

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -1,4 +1,55 @@
 package org.example;
-
 public class Juego {
+    private Jugador jugador;
+    private Crupier crupier;
+    private Baraja baraja;
+
+    public Juego(Jugador jugador) {
+        this.jugador = jugador;
+        this.crupier = new Crupier();
+    }
+
+    public void iniciarRonda() {
+        baraja = new Baraja();
+        baraja.barajar();
+        jugador.reiniciarMano();
+        crupier.reiniciarMano();
+    }
+
+    public void repartirInicial() {
+        jugador.pedirCarta(baraja);
+        jugador.pedirCarta(baraja);
+
+        crupier.getMano().agregarCarta(baraja.repartirCarta());
+        crupier.getMano().agregarCarta(baraja.repartirCarta());
+    }
+
+    public String determinarGanador() {
+        int valorJugador = jugador.getMano().calcularValor();
+        int valorCrupier = crupier.getMano().calcularValor();
+        int apuesta = jugador.getApuesta().getCantidad();
+
+        if (jugador.getMano().estaBust()) {
+            return "Te has pasado de 21. ¡Gana el Crupier!";
+        } else if (crupier.getMano().estaBust()) {
+            jugador.setCreditos(jugador.getCreditos() + (apuesta * 2));
+            return "El Crupier se ha pasado. ¡Has ganado!";
+        } else if (valorJugador > valorCrupier) {
+            jugador.setCreditos(jugador.getCreditos() + (apuesta * 2));
+            return "¡Tienes mejor puntuación! Has ganado.";
+        } else if (valorCrupier > valorJugador) {
+            return "El Crupier tiene mejor puntuación. ¡Gana el Crupier!";
+        } else {
+            jugador.setCreditos(jugador.getCreditos() + apuesta);
+            return "¡Empate! Se devuelve tu apuesta.";
+        }
+    }
+
+    public Baraja getBaraja() {
+        return baraja;
+    }
+
+    public Crupier getCrupier() {
+        return crupier;
+    }
 }
