b9ce711c6d2bd45b7413eb09e57f2c2e22b5d67f
oscareduchav <oscareduchav@gmail.com>
2026-04-12 11:56:46 +0200

    commit del main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
+package org.example;
 public class Main {
-   public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        ControladorJuego controlador = new ControladorJuego();
+        controlador.jugar();
     }
 }
