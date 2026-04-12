b2a2bab736c593e06d600953ce493e89ef4d5823
oscareduchav <oscareduchav@gmail.com>
2026-04-12 11:47:23 +0200

    commit Mano

--- src/main/java/org/example/Mano.java
+++ src/main/java/org/example/Mano.java
@@ -1,4 +1,46 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mano {
+    private List<Carta> cartas;
+
+    public Mano() {
+        cartas = new ArrayList<>();
+    }
+
+    public void agregarCarta(Carta carta) {
+        cartas.add(carta);
+    }
+
+    public int calcularValor() {
+        int total = 0;
+        int ases = 0;
+
+        for (Carta c : cartas) {
+            total += c.getValorNumerico();
+            if (c.getValor().equals("A")) {
+                ases++;
+            }
+        }
+
+        while (total > 21 && ases > 0) {
+            total -= 10;
+            ases--;
+        }
+
+        return total;
+    }
+
+    public boolean tieneBlackjack() {
+        return cartas.size() == 2 && calcularValor() == 21;
+    }
+
+    public boolean estaBust() {
+        return calcularValor() > 21;
+    }
+
+    public List<Carta> getCartas() {
+        return cartas;
+    }
 }
