c03cbf1e62a9a50eaea70c8dfa80864b414a0bc7
oscareduchav <oscareduchav@gmail.com>
2026-04-12 11:44:38 +0200

    commit de ControladoJuego

--- src/main/java/org/example/ControladorJuego.java
+++ src/main/java/org/example/ControladorJuego.java
@@ -1,4 +1,83 @@
 package org.example;
 
 public class ControladorJuego {
+    private Juego juego;
+    private VistaJuego vista;
+
+    public ControladorJuego() {
+        this.vista = new VistaJuego();
+    }
+
+    public void jugar() {
+        Jugador jugador = new Jugador("Jugador 1", 1000);
+        juego = new Juego(jugador);
+
+        vista.mostrarMensaje("¡Bienvenido al Casino Blackjack!");
+
+        while (jugador.getCreditos() > 0) {
+            int cantidadApuesta = vista.pedirApuesta(jugador.getCreditos());
+
+            if (cantidadApuesta <= 0 || cantidadApuesta > jugador.getCreditos()) {
+                vista.mostrarMensaje("Apuesta no válida.");
+                continue;
+            }
+
+            jugador.realizarApuesta(cantidadApuesta);
+            juego.iniciarRonda();
+            juego.repartirInicial();
+
+            boolean plantado = false;
+            boolean turnoTerminado = false;
+
+            // Turno del Jugador
+            while (!jugador.getMano().estaBust() && !plantado && !turnoTerminado) {
+                vista.mostrarManos(jugador, juego.getCrupier(), false);
+
+                if (jugador.getMano().tieneBlackjack()) {
+                    vista.mostrarMensaje("¡BLACKJACK!");
+                    turnoTerminado = true;
+                    continue;
+                }
+
+                String accion = vista.pedirAccion();
+
+                switch (accion) {
+                    case "1":
+                        jugador.pedirCarta(juego.getBaraja());
+                        break;
+                    case "2":
+                        plantado = true;
+                        break;
+                    case "3":
+                        if (jugador.getCreditos() >= jugador.getApuesta().getCantidad()) {
+                            jugador.doblar();
+                            jugador.pedirCarta(juego.getBaraja());
+                            turnoTerminado = true; // Solo una carta al doblar
+                        } else {
+                            vista.mostrarMensaje("No tienes créditos suficientes para doblar.");
+                        }
+                        break;
+                    default:
+                        vista.mostrarMensaje("Opción incorrecta.");
+                        break;
+                }
+            }
+
+            // Turno del Crupier
+            if (!jugador.getMano().estaBust()) {
+                vista.mostrarMensaje("\nTurno del Crupier...");
+                juego.getCrupier().jugarTurno(juego.getBaraja());
+            }
+
+            // Fin de la ronda
+            vista.mostrarManos(jugador, juego.getCrupier(), true);
+            String resultado = juego.determinarGanador();
+            vista.mostrarMensaje("\n" + resultado);
+            vista.mostrarMensaje("----------------------------------------\n");
+
+            if (jugador.getCreditos() <= 0) {
+                vista.mostrarMensaje("Te has quedado sin créditos. ¡Fin del Juego!");
+            }
+        }
+    }
 }
