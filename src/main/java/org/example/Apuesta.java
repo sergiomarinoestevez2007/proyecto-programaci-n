83e435b3f112e715c36f254c4c713f774527f1e3
oscareduchav <oscareduchav@gmail.com>
2026-04-12 11:43:49 +0200

    commit de Apuesta

--- src/main/java/org/example/Apuesta.java
+++ src/main/java/org/example/Apuesta.java
@@ -1,4 +1,12 @@
 package org.example;
-
 public class Apuesta {
+    private int cantidad;
+
+    public Apuesta(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
