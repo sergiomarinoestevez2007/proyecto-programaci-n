7cfdb5e2c6a2b7b52e99981dfdedc736bd9d9373
Sergio Mariño Estévez <sermaes2007@gmail.com>
2026-05-20 09:40:22 +0200

    "extends para la clase hija de participante en crupier con todo modificado de manera que borramos la mano y todo relacion porq lo hereda directamente de participante"

--- src/main/java/org/example/Crupier.java
+++ src/main/java/org/example/Crupier.java
@@ -1,29 +1,17 @@
 package org.example;
 
-public class Crupier {
-    private Mano mano;
+public class Crupier extends Participante{
 
-    // Constructor
     public Crupier() {
-        this.mano = new Mano();
+        super();
     }
 
-    // Getter de la mano
-    public Mano getMano() {
-        return mano;
-    }
 
-    // Lógica del turno del crupier
     public void jugarTurno(Baraja baraja) {
         // El crupier pide cartas mientras tenga menos de 17
         while (mano.calcularValor() < 17) {
             Carta carta = baraja.repartirCarta();
             mano.agregarCarta(carta);
         }
-    }
-
-    // Reinicia la mano para una nueva ronda
-    public void reiniciarMano() {
-        this.mano = new Mano();
     }
 }
